621f7d289a5f80149ed1be5fffa44734bf9b6294
Rahil Kapur <76180346+rahilkapur@users.noreply.github.com>
2022-05-29 14:36:15 -0500

    Time: 2 ms (57.45%), Space: 73 MB (68.43%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.java
+++ 53-maximum-subarray/53-maximum-subarray.java
@@ -3,7 +3,7 @@ class Solution {
         int len = nums.length;
         int localMax = 0;
         int max = Integer.MIN_VALUE;
-        for (int i = 0; i < len; i++) {
+        for (int i = 0; i < nums.length; i++) { //kadanes algo we check to see if the local max is either nums[i] or if it is nums[i] + local max of nums[i - 1] making our algo O(n) since we're going thru the array only once finding our max everytime not needing to go back and check constantly
             localMax = Math.max(nums[i], nums[i] + localMax);
             if (localMax > max) {
                 max = localMax;
